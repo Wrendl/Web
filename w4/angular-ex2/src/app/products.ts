f24a9b84ff6f906269a24cde014a1b5ac218c9e9
Wrendl <narimanganiyev25@gmail.com>
2020-02-18 15:04:53 +0600

    123

---
+++ w4/angular-ex2/src/app/products.ts
@@ -0,0 +1,76 @@
+export interface Product{
+  name: string,
+  rating: number,
+  description: string,
+  ll: string
+}
+
+export const products = [
+  {
+    name: 'Asus ROG STRIX-GTX1050TI-4G-GAMING',
+    rating: 5.0,
+    description: 'Бренд: ASUS Модель графического процессора: GeForce GTX 1050 Ti Состояние товара: Бывший в употреблении Тип охладителя: Вентилятор Производитель чипсета: NVIDIA Тип выходного интерфейса: DVI Тип выходного интерфейса: HDMI',
+    ll: "https://aliexpress.ru/item/10000072636564.html?spm=a2g0o.productlist.0.0.47221881pXVPpZ&algo_pvid=16f20843-c6b3-4ed7-8852-2b2fc7126598&algo_expid=16f20843-c6b3-4ed7-8852-2b2fc7126598-0&btsid=0ab6f82215815009381214596e26d0&ws_ab_test=searchweb0_0,searchweb201602_,searchweb201603_"
+  },
+  {
+    name: 'PALIT PCIE16 GT710 2GB GDDR3 PA-GT710-2GD3H',
+    rating: 5.0,
+    description: 'Бренд: Palit Модель графического процессора: GeForce GT 710 Объем видеопамяти: 2 ГБ Состояние товара: Новый Тип интерфейса: Компьютерная шина PCI Express Тип охладителя: Вентилятор Тип видеопамяти: GDDR3 Производитель чипсета: NVIDIA',
+    ll: "https://aliexpress.ru/item/4000275242127.html?spm=a2g0o.productlist.0.0.47221881pXVPpZ&algo_pvid=16f20843-c6b3-4ed7-8852-2b2fc7126598&algo_expid=16f20843-c6b3-4ed7-8852-2b2fc7126598-1&btsid=0ab6f82215815009381214596e26d0&ws_ab_test=searchweb0_0,searchweb201602_,searchweb201603_"
+  },
+  {
+    name: 'Yeston Radeon RX 550 GPU 4 ГБ GDDR5 128 бит',
+    rating: 4.9,
+    description: 'Бренд: yeston Модель графического процессора: Radeon RX550 Объем видеопамяти: 4 ГБ Состояние товара: Новый Тип интерфейса: Компьютерная шина PCI Express Тип охладителя: Вентилятор Тип видеопамяти: GDDR5',
+    ll: "https://aliexpress.ru/item/32851784021.html?spm=a2g0o.productlist.0.0.47221881pXVPpZ&algo_pvid=16f20843-c6b3-4ed7-8852-2b2fc7126598&algo_expid=16f20843-c6b3-4ed7-8852-2b2fc7126598-3&btsid=0ab6f82215815009381214596e26d0&ws_ab_test=searchweb0_0,searchweb201602_,searchweb201603_"
+  },
+  {
+    name: 'Видеокарта RX 570 DirectX 12 8GB 256-Bit GDDR5',
+    rating: 4.8,
+    description: 'Бренд: VEINEDA Состояние товара: Новый Тип интерфейса: Компьютерная шина PCI Express 3.0 X16 Модель графического процессора: Radeon RX570 Объем видеопамяти: 8 Гб Тип охладителя: Вентилятор Тип видеопамяти: GDDR5',
+    ll:"https://aliexpress.ru/item/4000223387448.html?spm=a2g0o.productlist.0.0.47221881pXVPpZ&algo_pvid=16f20843-c6b3-4ed7-8852-2b2fc7126598&algo_expid=16f20843-c6b3-4ed7-8852-2b2fc7126598-6&btsid=0ab6f82215815009381214596e26d0&ws_ab_test=searchweb0_0,searchweb201602_,searchweb201603_"
+  },
+  {
+    name: 'Видеокарта Gigabyte Gaming nVidia GeForce GTX 1660 Super',
+    rating: 4.9,
+    description: 'Бренд: Gigabyte Модель графического процессора: GTX 1660 Объем видеопамяти: 6 ГБ Состояние товара: Новый Тип интерфейса: Компьютерная шина PCI Express 3.0 X16',
+    ll:"https://aliexpress.ru/item/4000513063064.html?spm=a2g0o.productlist.0.0.47221881pXVPpZ&algo_pvid=16f20843-c6b3-4ed7-8852-2b2fc7126598&algo_expid=16f20843-c6b3-4ed7-8852-2b2fc7126598-9&btsid=0ab6f82215815009381214596e26d0&ws_ab_test=searchweb0_0,searchweb201602_,searchweb201603_"
+  },
+  {
+    name: 'Yeston Radeon RX 580 GPU 8 ГБ GDDR5 256bit',
+    rating: 4.6,
+    description: 'Бренд: yeston Модель графического процессора: Radeon RX580 Объем видеопамяти: 8 Гб Состояние товара: Новый Тип интерфейса: Компьютерная шина PCI Express 3.0 X16',
+    ll:"https://aliexpress.ru/item/32950380764.html?spm=a2g0o.productlist.0.0.47221881pXVPpZ&algo_pvid=16f20843-c6b3-4ed7-8852-2b2fc7126598&algo_expid=16f20843-c6b3-4ed7-8852-2b2fc7126598-13&btsid=0ab6f82215815009381214596e26d0&ws_ab_test=searchweb0_0,searchweb201602_,searchweb201603_"
+  },
+  {
+    name: 'Видеокарта ASUS ROG nVidia GeForce GTX 1050 Ti ',
+    rating: 5.0,
+    description: 'Бренд: Asus Модель графического процессора: GeForce GTX 1050 Ti Объем видеопамяти: 4 ГБ Состояние товара: Новый Тип интерфейса: Компьютерная шина PCI Express 3.0 X16',
+    ll:"https://aliexpress.ru/item/4000603295793.html?spm=a2g0o.productlist.0.0.47221881pXVPpZ&algo_pvid=16f20843-c6b3-4ed7-8852-2b2fc7126598&algo_expid=16f20843-c6b3-4ed7-8852-2b2fc7126598-12&btsid=0ab6f82215815009381214596e26d0&ws_ab_test=searchweb0_0,searchweb201602_,searchweb201603_"
+  },
+  {
+    name: 'Yeston Radeon mini RX 550 GPU 4 ГБ GDDR5 128 бит',
+    rating: 4.7,
+    description: 'Бренд: yeston Модель графического процессора: Radeon RX550 Объем видеопамяти: 4 ГБ Состояние товара: Новый Тип интерфейса: Компьютерная шина PCI Express 3.0 X16',
+    ll:"https://aliexpress.ru/item/33014189507.html?spm=a2g0o.productlist.0.0.47221881pXVPpZ&algo_pvid=16f20843-c6b3-4ed7-8852-2b2fc7126598&algo_expid=16f20843-c6b3-4ed7-8852-2b2fc7126598-15&btsid=0ab6f82215815009381214596e26d0&ws_ab_test=searchweb0_0,searchweb201602_,searchweb201603_"
+  },
+  {
+    name: 'Yeston RX550--4G D5 Видеокарты Radeon Chill 4 Гб',
+    rating: 5.0,
+    description: 'Бренд: yeston Состояние товара: Новый Тип интерфейса: Компьютерная шина PCI Express 3.0 X16 Модель графического процессора: Radeon RX550 Объем видеопамяти: 4 ГБТип охладителя: Вентилятор Тип видеопамяти: GDDR5',
+    ll:"https://aliexpress.ru/item/4000072077229.html?spm=a2g0o.productlist.0.0.47221881pXVPpZ&algo_pvid=16f20843-c6b3-4ed7-8852-2b2fc7126598&algo_expid=16f20843-c6b3-4ed7-8852-2b2fc7126598-28&btsid=0ab6f82215815009381214596e26d0&ws_ab_test=searchweb0_0,searchweb201602_,searchweb201603_"
+  },
+  {
+    name: 'SAPPHIRE RX 560 4 Гб видеокарта GPU Radeon RX 560D 4G ',
+    rating: 4.8,
+    description: 'Бренд: SAPPHIRE Состояние товара: Бывший в употреблении Тип интерфейса: Компьютерная шина PCI Express 3.0 X16 Объем видеопамяти: 4 ГБ Модель графического процессора: Radeon RX560',
+    ll:"https://aliexpress.ru/item/4000226277534.html?spm=a2g0o.productlist.0.0.47221881pXVPpZ&algo_pvid=16f20843-c6b3-4ed7-8852-2b2fc7126598&algo_expid=16f20843-c6b3-4ed7-8852-2b2fc7126598-29&btsid=0ab6f82215815009381214596e26d0&ws_ab_test=searchweb0_0,searchweb201602_,searchweb201603_"
+  }
+];
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
